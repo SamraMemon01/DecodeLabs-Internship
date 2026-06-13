02b4ae7e935913043ecad6a9f9dd3603705d5712
SamraMemon01 <samramemon60@gmail.com>
2026-06-14 00:10:01 +0500

    Add files via upload

--- week-1-project/PasswordStrengthChecker.py
+++ week-1-project/PasswordStrengthChecker.py
@@ -1,2 +1,38 @@
-print("hello")
-print("hi")
+import string
+
+password = input("Enter a password: ")
+
+length = len(password)
+
+has_upper = False
+has_digit = False
+has_symbol = False
+
+for char in password:
+    if char.isupper():
+        has_upper = True
+    elif char.isdigit():
+        has_digit = True
+    elif char in string.punctuation:
+        has_symbol = True
+
+score = 0
+
+if length >= 8:
+    score += 1
+
+if has_upper:
+    score += 1
+
+if has_digit:
+    score += 1
+
+if has_symbol:
+    score += 1
+
+if score <= 1:
+    print("Password Strength: Weak")
+elif score <= 3:
+    print("Password Strength: Medium")
+else:
+    print("Password Strength: Strong")
